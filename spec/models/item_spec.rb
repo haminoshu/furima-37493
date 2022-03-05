80184c2a5954442a0b32b4095e5597a64c110327
haminoshu <hamichan1104@gmail.com>
2022-03-05 14:39:02 +0900

    商品出品機能③

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,19 +8,10 @@ RSpec.describe Item, type: :model do
   
     describe '商品出品機能' do
       context '商品出品できるとき' do
-        it 'items_name、explain、category_id、status_id、shipping_fee_burden_id、prefecture_id、delivery_day_id、items_price、imageが存在すれば登録できる' do
+        it 'items_name、items_price、explain、category_id、status_id、shipping_fee_burden_id、prefecture_id、delivery_day_id、items_price、imageが存在すれば登録できる' do
           expect(@item).to be_valid
         end
-
-        it '300円以上であれば登録できる' do
-          @item.items_price >= 300
-          expect(@item).to be_valid
-        end
-        
-        it '9999999円以下であれば登録できる' do
-          @item.items_price <= 9999999
-          expect(@item).to be_valid
-        end
+      
       end
       
       context '商品出品できないとき' do
@@ -46,7 +37,17 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Image can't be blank")
         end
      
+        it '300円以上でなければ登録できない' do
+          @item.items_price = 299
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Items price must be greater than or equal to 300")
+        end
         
+        it '9999999円以下でなければ登録できない' do
+          @item.items_price = 10000000
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Items price must be less than or equal to 9999999")
+        end
 
       end
   end
