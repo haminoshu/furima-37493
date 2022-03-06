72393b01915c807e8a7b3d4e45457837c4dd1de0
haminoshu <hamichan1104@gmail.com>
2022-03-06 23:35:00 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,8 +21,23 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-  end  
+  end
 
+  def edit
+    @item = Item.find(params[:id])
+
+    redirect_to action: :index unless current_user == @item.user
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   private
 
