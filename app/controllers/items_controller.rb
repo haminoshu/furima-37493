89629d0324d5da22f6b2a3c5eaad7367457a3c42
haminoshu <hamichan1104@gmail.com>
2022-03-07 15:59:23 +0900

    商品削除機能②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
@@ -43,10 +43,6 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     item.destroy
     redirect_to root_path
-
-    unless user_signed_in?
-      redirect_to action: :root_path
-    end
   end
 
 
