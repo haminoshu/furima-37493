284b52ba026902508e6e0b66f3efd9a8aeb694eb
haminoshu <hamichan1104@gmail.com>
2022-03-12 14:36:36 +0900

    商品購入機能⑤

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -9,6 +9,8 @@ class BuysController < ApplicationController
 
     redirect_to root_path if current_user == @item.user
 
+    redirect_to  new_user_session_path unless user_signed_in?
+
   end
 
   def new
